e44cb6a1cf269092fb57f9c482b081fca7533ff4
Tisdale Fry <tisdalefry@gmail.com>
2020-02-25 03:12:57 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -1,11 +1,17 @@
+require "pry"
+#binding.pry
 BASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
 }
 
-
 def add_character(show, name)
+  
   # Write your implementation here
   # Should return the array of the 'show' argument
 end
+binding.pry
+add_character(chipmunks, Snarf)
+add_character(third_earthers, Snarf)
+add_character(jetsons, Snarf)
